c86df6a34bded893e56cef6af434abbd2ce0577a
Emmanuel Shine joseph <73063606+emshine@users.noreply.github.com>
2023-01-16 11:59:26 +0530

    Create Input and Output.java

---
+++ Basic Programs/Input and Output.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class InputandOutput{
+    public static void main(String[]args){
+        System.out.println("Enter the number:");
+        Scanner a = new Scanner(System.in);
+        int b = a.nextInt();
+        System.out.println("The entered number is"+b);
+    }
+}
+
+
+--------------------------------------------------------------------
+Output::::
+java InputandOutput
+Enter the number:
+67
+The entered number is67
